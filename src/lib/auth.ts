071fcc796c7058ff140ce762bdc6a405a202a548
Jeong min Lee <132313665+JeongMin-Lee3@users.noreply.github.com>
2025-05-07 17:45:24 +0900

    Delete src directory

--- src/lib/auth.ts
+++
@@ -1,9 +0,0 @@
-export function getLoggedInUser() {
-	const user = localStorage.getItem('user');
-	return user ? JSON.parse(user) : null;
-}
-
-export function logout() {
-	localStorage.removeItem('user');
-	window.location.href = '/login';
-}
